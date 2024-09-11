638b37651c75dc6ace7c626144db4beac9bc8090
Kathryn Graham <Kate796@hotmail.com>
2024-09-11 09:52:39 +0000

    a

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -1,19 +1,13 @@
-with 
-
-source as (
-
+with source as (
     select * from {{ source('raw', 'product') }}
-
 ),
 
 renamed as (
-
-     select
-          products_id,
-          CAST(purchse_price as FLOAT64) as purchase_price
-
+    select
+        product_id,
+        CAST(purchase_price as FLOAT64) as purchase_price
     from source
-
 )
 
 select * from renamed
+
